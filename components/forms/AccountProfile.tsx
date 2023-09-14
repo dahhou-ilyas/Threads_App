0bd0df2ed117811c303535764af2934bfdd5e1c7
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-09-14 01:04:05 +0100

    Create a Thread model with its own method

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -150,6 +150,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   onChange={(e) => handleImage(e, field.onChange)}
                 />
               </FormControl>
+              <FormMessage/>
             </FormItem>
           )}
         />
---
+++ lib/actions/thread.actions.ts
@@ -0,0 +1,17 @@
+import Thread from "../models/Thread.model";
+import { connectToDB } from "../mongoose";
+
+interface Params{
+    text:string,
+    author:string,
+    communityId:string | null;
+    path:string
+}
+export async function createThread({
+text,author,communityId,path
+}:Params){
+    connectToDB();
+    const createThread=await Thread.create({
+        text,author,community:communityId
+    });
+}
