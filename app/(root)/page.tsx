fd87360ed3cd4da9ba7b4060128a3c548ccc4d88
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-09-16 10:55:06 +0100

    complete thread card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
-      <section className="mt-9 flex flex-col gap-0">
+      <section className="mt-9 flex flex-col gap-3">
         {
           resutl.posts.length==0 ? (
             <p className="no-result">No Threads Found</p>
