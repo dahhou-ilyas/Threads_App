8bf8ae1ab19d590b2cf68588236bce8ddf633154
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-09-16 13:15:27 +0100

    fetchThreadById query

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,7 +1,37 @@
+import ThreadCard from '@/components/cards/ThreadCard'
 import React from 'react'
+import { currentUser } from '@clerk/nextjs'
+import { fetchUser } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
+import { fetchThreadById } from '@/lib/actions/thread.actions';
+export default async function Page({params}:{params:{id:string}}) {
 
-export default function Page({params}:{params:{id:string}}) {
-  return (
-    <section></section>
-  )
+    if (!params.id) return null;
+    const user=await currentUser();
+
+
+    if(!user) return null;
+
+    const userInfo=await fetchUser(user.id)
+
+    if(!userInfo?.onboarded) redirect('/onboarding')
+
+    const thread=await fetchThreadById(params.id)
+
+    return (
+      <section className='relative'>
+          <div>
+              <ThreadCard key={thread._id}
+              id={thread._id}
+              currentUserId={user?.id || ""}
+              parentId={thread.parentId}
+              content={thread.text}
+              author={thread.author}
+              community={thread.community}
+              createdAt={thread.createAt}
+              comments={thread.childrent}
+              />
+          </div>
+      </section>
+    )
 }
